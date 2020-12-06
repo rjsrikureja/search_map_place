3d5da20e894f5d3783bef4c7d6ddf3535428ec52
rjsrikureja <rjsrikureja@gmail.com>
2020-12-06 18:24:15 +0700

    change aniimation

--- lib/src/searchMapPlaceWidget.dart
+++ lib/src/searchMapPlaceWidget.dart
@@ -99,7 +99,7 @@ class _SearchMapPlaceWidgetState extends State<SearchMapPlaceWidget> with Ticker
   void initState() {
     geocode = Geocoding(apiKey: widget.apiKey, language: widget.language);
     _animationController = AnimationController(vsync: this, duration: Duration(milliseconds: 500));
-    _containerHeight = Tween<double>(begin: 55, end: 110).animate(
+    _containerHeight = Tween<double>(begin: 55, end: 65).animate(
       CurvedAnimation(
         curve: Interval(0.0, 0.5, curve: Curves.easeInOut),
         parent: _animationController,
